44cbe91ca4e658d4b9ef1ddb0b027bef323f90f1
Enether <familyguyuser192@windowslive.com>
2017-02-03 14:23:08 +0200

    Added more quicksort versions, NEEDS REFACTORING

--- Coursera/algorithms_stanford/Quick Sort/quicksort.py
+++ Coursera/algorithms_stanford/Quick Sort/quicksort.py
@@ -59,7 +59,7 @@ def _non_random_quick_sort(array, start, end):
         j += 1
 
     # recursion up until we hit partition every part
-    _non_random_quick_sort(array, start, pivot_idx)  # do the same for the left part
+    _non_random_quick_sort(array, start, pivot_idx+1)  # do the same for the left part
     _non_random_quick_sort(array, pivot_idx+1, end)  # do the same for the right part
 
 
@@ -79,15 +79,64 @@ def _non_random_quick_sort_end_pivot(array, start, end):
             i = pivot_idx - 1
         j -= 1
     _non_random_quick_sort_end_pivot(array, start, pivot_idx - 1)
-    _non_random_quick_sort_end_pivot(array, pivot_idx, end)
+    _non_random_quick_sort_end_pivot(array, pivot_idx+1, end)
+
+
+def _non_random_quick_sort_median_pivot(array, start, end):
+    length = (end-start)+1
+    if end <= start:
+        return
+    med_idx = (length//2)-1 if length % 2 == 0 else length//2
+    med = array[med_idx]
+    first = array[start]
+    end_el = array[end]
+    piv_index = {
+        med: med_idx,
+        first: start,
+        end_el: end
+    }
+    # swap with start
+    median = piv_index[list(sorted([med, first, end_el]))[1]]
+    array[start], array[median] = array[median], array[start]
+
+    pivot_idx = start
+    pivot_item = array[pivot_idx]
+    i, j = start + 1, start + 1
+    # i points to the first item after pivot
+    # j points to the element we're at
+
+    while j <= end:
+        if array[j] < pivot_item:  # element after pivot is smaller, we need to swap
+            # swap with element after pivot
+            # ex: pivot=5, i=10, [2, 3, 5, 10, 1]
+            array[i], array[j] = array[j], array[i]
+            # ex: [2, 3, 5, 1, 10]
+            # swap with pivot
+            array[pivot_idx], array[i] = array[i], array[pivot_idx]
+            # ex: [2, 3, 1, 5, 10]
+            pivot_idx = i
+            i = pivot_idx + 1
+        j += 1
+
+    # recursion up until we hit partition every part
+    _non_random_quick_sort_median_pivot(array, start, pivot_idx-1)  # do the same for the left part
+    _non_random_quick_sort_median_pivot(array, pivot_idx + 1, end)  # do the same for the right part
 
 
 def non_random_quick_sort(array: list):
     _non_random_quick_sort(array, 0, len(array)-1)
     return array
 
+
 def non_random_quick_sort_end_pivot(array: list):
     _non_random_quick_sort_end_pivot(array, 0, len(array)-1)
     return array
 
+
+def non_random_quick_sort_median_pivot(array: list):
+    _non_random_quick_sort_median_pivot(array, 0, len(array)-1)
+    return array
+
+print(non_random_quick_sort_median_pivot([3, 3, 25, 4, -1120, 1, 2, -3, -10, 250, -402]))
+print(non_random_quick_sort([3, 3, 25, 4, -1120, 1, 2, -3, -10, 250, -402]))
 print(non_random_quick_sort_end_pivot([3, 3, 25, 4, -1120, 1, 2, -3, -10, 250, -402]))
