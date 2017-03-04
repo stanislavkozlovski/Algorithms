e959d17f5b94047cd7dff6e31955cd48e8c1c752
Enether <familyguyuser192@windowslive.com>
2017-03-05 00:18:06 +0200

    Added Floyd-Warshall

---
+++ SoftUni-Algorithms-April-2016/Advanced Graph Algorithms/Exercise/Python/floyd_warshall.py
@@ -0,0 +1,23 @@
+from copy import deepcopy
+
+graph = [[0, 16, 13, 0, 0, 0],
+         [0, 0, 10, 12, 0, 0],
+         [0, 4, 0, 0, 14, 0],
+         [0, 0, 9, 0, 0, 20],
+         [0, 0, 0, 7, 0, 4],
+         [0, 0, 0, 0, 0, 0]]
+distances = []
+
+for idx, row in enumerate(graph):
+    new_row = []
+    for idx_2, col in enumerate(row):
+        new_row.append(col if col != 0 else float('Inf'))
+    distances.append(new_row)
+
+for k in range(len(graph)):
+    for i in range(len(graph)):
+        for j in range(len(graph)):
+            new_distance = distances[i][k] + distances[k][j]
+            if new_distance < distances[i][j]:
+                distances[i][j] = new_distance
+print(distances<)
